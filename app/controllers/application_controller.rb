8d144db4ff375261b9fe6713f1b04b08bd58cf65
Nathaniel Ram <lilram99@hotmail.com>
2015-06-07 23:42:56 -0400

    Really not sure how to turn this into one line since it's expecting solely a symbol. This should work for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  check_authorization :unless => (:devise_controller? || :rails_admin_controller?)
+  check_authorization :unless => :devise_controller?
+  check_authorization :unless => :rails_admin_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
   	#Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
