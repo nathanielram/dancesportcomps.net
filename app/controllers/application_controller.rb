0fd483f4058afb8a2dba0fe6419f73eab884a791
Nathaniel Ram <lilram99@hotmail.com>
2015-06-07 23:58:06 -0400

    RailsAdmin hack fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   check_authorization :unless => :devise_controller?
-  check_authorization :unless => :rails_admin_controller?
+  #check_authorization :unless => :rails_admin_controller?
+  check_authorization :unless => :is_rails_admin_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
   	#Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
     #redirect_to root_url, :alert => exception.message
     render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
+
+  private
+  # hack fix -- https://github.com/sferik/rails_admin/issues/2268
+  def is_rails_admin_controller?
+	(self.class.to_s =~ /RailsAdmin::/) == 0
   end
 
 end
