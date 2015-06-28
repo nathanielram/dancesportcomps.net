9621dbf6bcc9a95853176ce78c5bba192e1a5fca
Nathaniel Ram <lilram99@hotmail.com>
2015-06-28 01:56:29 -0400

    Scopes are fantastic

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -16,7 +16,7 @@ class Competition < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug_candidates, use: [:slugged, :finders]
 
-  has_many :occurences, dependent: :destroy
+  has_many :occurences, -> { order(start_date: :desc) }, dependent: :destroy
   has_many :locations, through: :occurences
 
   #accepts_nested_attributes_for :occurences
